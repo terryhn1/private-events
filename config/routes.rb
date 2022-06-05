19580a15becf1b19ae203527559ff88fae581bab
terryhn1 <57304245+terryhn1@users.noreply.github.com>
2022-06-05 12:08:58 -0700

    2.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => {registrations: "registrations"}
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  resources :events
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "events#index"
 end
